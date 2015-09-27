6f705ca2190a989471e5a0a1eee6df734c44d303
estruch18 <estruch18@192.168.1.135>
2015-09-27 12:22:07 +0200

    Avance proyecto1c AD
    Signed-off-by: estruch18 <estruch18@192.168.1.135>

--- Proyecto1c/src/Funciones/FuncionesImpl.java
+++ Proyecto1c/src/Funciones/FuncionesImpl.java
@@ -14,74 +14,97 @@ public class FuncionesImpl {
 	private boolean iguales = true;
 	
 	public FuncionesImpl(){
-		//DECLARACIÓN DE 2 FICHEROS VACIOS
-		
+		//DECLARACIÓN DE 2 FICHEROS
 		f1 = new File("fichero1.txt");
 		f2 = new File("fichero2.txt");
 		
 		//this.compararContenido(f1, f2);
-		this.buscarPalabra(f2, "Hola", true);
+		this.buscarPalabra(f1, "uno", true);
 	}
 	
 	
 	public boolean compararContenido(File fichero1, File fichero2){
 		
+		String cadena1=null, cadena2=null;
 		try{
 			
 			FileReader fr1 = new FileReader(f1);
 			FileReader fr2 = new FileReader(f2);
-			
-			
 			BufferedReader bf1 = new BufferedReader(fr1);
 			BufferedReader bf2 = new BufferedReader(fr2);
-			
-			String cadena1, cadena2;
-			
+			//Empezamos a leer 
 			cadena1 = bf1.readLine();
 			cadena2 = bf2.readLine();
 			
 			while ((cadena1!=null) && (cadena2!=null) && iguales==true) {
-				//COMPROBAMOS QUE LAS LINEAS NO SON IGUALES
-				  if (cadena1.equals(cadena2)==false){
-					  	iguales = false;
-					  	System.out.println("Ficheros no iguales.");
-				  		return false;
-				 } 
+				//Compración de si ambas lineas son iguales o no
+				
+				  if (cadena1.equals(cadena2)==true){
+					//Si las dos lineas coinciden y son iguales seguimos leyendo para comprobar
+					  cadena1=bf1.readLine();
+					  cadena2=bf2.readLine();
+				  } 
 				  else{
-					  System.out.println("Ficheros iguales.");
-				  }
-				  //SEGUIMOS LEYENDO
-				  cadena1=bf1.readLine();
-				  cadena2=bf2.readLine();
+					   //Las dos cadenas son diferentes, por tanto, modificamos iguales a false para posteriormente condicionarlo y devolvemos false
+					  	iguales = false;
+				  		return false;
+				  }  
 			}
 		}
 		catch(IOException error){
 			System.err.println("Error al comparar los ficheros.");
 		}
 		
+		if(cadena1==null && cadena2==null && iguales==true){
+			System.out.println("Ficheros iguales");
+		}
+		else{
+			System.out.println("Ficheros no iguales");
+		}
+		
 		return true;
 	}
 	
+	
+	
 	public int buscarPalabra(File fichero, String palabra, boolean boleano){
 		
+		String cadena=null;
+		boolean palabraEncontrada=false;
 		try{
-			FileReader fr1 = new FileReader(f1);
-			BufferedReader bf1 = new BufferedReader(fr1);
 			
-			String linea = bf1.readLine();
-			
-			while(linea!=null){
-	            System.out.println(linea);
-	            
-	            if(linea.equals(palabra)==true){
-	            	System.out.println(linea+" - Palabra encontrada.");
-	            	break;
-	            }
-	            linea = bf1.readLine();
+			if(fichero.exists()==false){
+				System.out.println("El fichero no existe.");
+				return -1;
+			}
+			else{
+				
+				FileReader fr1 = new FileReader(fichero);
+				BufferedReader bf1 = new BufferedReader(fr1);
+				
+				cadena = bf1.readLine();
+				
+				while(cadena!=null){
+					
+		            if(cadena.equals(palabra)==true){
+		            	palabraEncontrada=true;
+		            	break;
+		            }
+		            else{
+		            	cadena = bf1.readLine();
+		            } 
+				}
 			}
 		}
 		catch(IOException error){
 			System.err.println("Error al encontrar la palabra");
+		}
+		
+		if(palabraEncontrada==true){
+			System.out.println("Se ha encontrado la palabra: "+palabra);
+		}
+		else{
+			System.out.println("No se ha encontrado la palabra: "+palabra);
 		}
 		
 		return 5;
